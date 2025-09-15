07f461f7a726288f532d6a78336aa8c164f6c6df
Lenny <mubealenny@gmail.com>
2025-09-15 23:19:29 +0300

    OOP

---
+++ Password example/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class UserAccount
+{
+    public string Username { get; set; }
+
+    public string Password { private get; set; }
+
+    public UserAccount(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+
+    public bool VerifyPassword(string input)
+    {
+        return Password == input;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        UserAccount account = new UserAccount("lennymubea", "Lenny2025");
+
+        Console.WriteLine("Username: " + account.Username);
+
+        bool correct = account.VerifyPassword("Lenny2005");
+        Console.WriteLine("Password: " + correct);
+
+        bool wrong = account.VerifyPassword("joemama1");
+        Console.WriteLine("Password: " + wrong);
+
+        account.Password = "joemama1";
+        bool status = account.VerifyPassword("joemama1");
+        Console.WriteLine("Password: " + status);
+        
+    }
+}
