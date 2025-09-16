e2c50d8a11bc47c5611bf1a7d0a63fe9d467b2d7
Lenny <mubealenny@gmail.com>
2025-09-16 21:53:50 +0300

    Task Manager

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class TaskList
+{
+    //attributes
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public int Priority {get; set;}
+    public bool IsCompleted {get; private set;}
+
+    //constructor
+    public void TaskList(string title, string description, int priority)
+    {
+        Title = title;
+        Description = description;
+        Priority = priority;
+        IsCompleted = false;
+    }
+
+    //methods
+    public void MarkComplete()
+    {
+        IsCompleted = true;
+        Console.WriteLine($"{Title} is now completed");
+    }
+
+    public void DisplayTask()
+    {
+        Console.WriteLine($"Title : {Title}" + $"Status: {IsCompleted}");
+        Console.WriteLine($"Description : {Description}");
+        Console.WriteLine($"Priority : {Priority}");
+
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TaskList taskItem1 = new TaskList("School work", "Finish assignment on Power Electronics", 3);
+        TaskList.DisplayTask();
+    }
+      
+}
